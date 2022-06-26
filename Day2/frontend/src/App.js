c44a5b29b4eb5856f05e1dad57c36e2add713d4d
lilixac <hinewton25@gmail.com>
2022-06-26 17:21:58 +0545

    Change address when user changes wallets

--- Day2/frontend/src/App.js
+++ Day2/frontend/src/App.js
@@ -24,6 +24,9 @@ const App = () => {
             if (window.ethereum === undefined) {
                 console.log("no wallet detected")
             }
+            window.ethereum.on("accountsChanged", function account() {
+                setAccount(window.ethereum.selectedAddress)
+            })
         }
         fetchData();
     }, [])
